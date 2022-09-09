5284e36e4c4f494e4b1a40e23058b8f2313055aa
colinmfoster4723 <colin.foster4723@gmail.com>
2022-09-09 17:30:41 -0500

    Initial commit

---
+++ src/lottery.js
@@ -0,0 +1,73 @@
+import web3 from "./web3";
+
+///when deploying contract console.log address and abi, paste both into variables
+
+const address = "0x95Cd26C3eD49fd410Fe31713D2BBe58ED667Ed2C";
+
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: "potValue",
+    outputs: [{ name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "manager",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getPlayers",
+    outputs: [{ name: "", type: "address[]" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+];
+
+//Create an instance of the contract, passing in the abi and the adddress
+//
+
+export default new web3.eth.Contract(abi, address);
